f320ad33f4e6bb55c046b73a7fd8fc42113fb136
Mariusz Piela <mariusz.piela@tmsolution.pl>
2017-03-29 15:55:06 +0200

    Merge origin/master
    Conflicts:
    	README.md

--- src/TMSolution/PrototypeBundle/Util/RequestAnalyzer.php
+++ src/TMSolution/PrototypeBundle/Util/RequestAnalyzer.php
@@ -2,40 +2,47 @@
 
 namespace TMSolution\PrototypeBundle\Util;
 
-
 class RequestAnalyzer {
 
     const APPLICATION_PATH = 'applicationPath';
-    const ENTITY_PATH = 'entityAlias';
-    const ENTITY_ALIAS = 'entityPath';
+    const ENTITIES_PATH = 'entitiesPath';
     const DELIMETER = '/';
-
+    
     protected $request;
+    protected $applicationMapper;
+    protected $entityMapper;
 
-    public function __construct() {
-       
+    public function __construct($applicationMapper, $entityMapper) {
+        $this->applicationMapper = $applicationMapper;
+        $this->entityMapper = $entityMapper;
     }
 
     public function analyze($request) {
-       
-        $requestAnalyze=new RequestAnalyze();
-        $applicationPath=$request->request->get(self::APPLICATION_PATH);
+        $requestAnalyze = new RequestAnalyze();
+        $applicationPath = $request->request->get(self::APPLICATION_PATH);
         $requestAnalyze->setApplicationPath($applicationPath);
-        $entityAlias=$request->request->get(self::ENTITY_PATH);
-        
+        $entitiesPath = $request->request->get(self::ENTITIES_PATH);
         $requestAnalyze->setEntityPath($this->getApplication());
-        $requestAnalyze->setEntityAlias($this->getEntityAlias());
+        $requestAnalyze->setEntityAlias($this->getEntityAlias($entitiesPath));
         return $requestAnalyze;
     }
 
     protected function getApplication($applicationPath) {
-        $applicationPathArr=explode(self::DELIMETER,$applicationPath);
+        $applicationPathArr = explode(self::DELIMETER, $applicationPath);
         return $applicationPathArr[0];
     }
-
+    
     protected function getEntityAlias($entitiesPath) {
-        $applicationPathArr=explode(self::DELIMETER,$applicationPath);
-        return end($applicationPathArr);         
+        $applicationPathArr = explode(self::DELIMETER, $entitiesPath);
+        return end($applicationPathArr);
+    }
+    
+    protected function getNamespace($application) {
+       return  $this->applicationMapper->getNamespaces($application);
+    }
+    
+    protected function getEntityClass($entityAlias,$namespaces) {   
+       return  $this->entityMapper->getClassName($entityAlias,$namespaces);
     }
 
 }
