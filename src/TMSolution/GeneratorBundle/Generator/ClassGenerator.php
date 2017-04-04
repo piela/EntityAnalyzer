390a3528db48e6f6fcf5779ed2b5f944bec64e39
Mariusz Piela <mariusz.piela@tmsolution.pl>
2017-04-04 14:55:39 +0200

    Merge origin/master
    Conflicts:
    	README.md

--- src/TMSolution/GeneratorBundle/Generator/ClassGenerator.php
+++ src/TMSolution/GeneratorBundle/Generator/ClassGenerator.php
@@ -29,9 +29,7 @@ abstract class ClassGenerator extends AbstractGenerator {
         return implode("\\", $entityNameArr);
     }
 
-    
-    
-    
+
     protected function getClassName()
     {
         $arr= explode(".",$this->getFileName());
